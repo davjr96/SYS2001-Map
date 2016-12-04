3941215bb8aca685f22c0e467a8fbf98e25b5c61
davjr96 <davjr96@gmail.com>
2016-12-04 17:49:38 -0500

    Style map

--- map.js
+++ map.js
@@ -1,7 +1,67 @@
  var map;
 
  function initialize(location) {
-     var mapOptions = {}
+     var mapOptions = {
+       styles: [{
+             "elementType": "labels",
+             "stylers": [{
+                 "visibility": "off"
+             }]
+         }, {
+             "featureType": "administrative.land_parcel",
+             "stylers": [{
+                 "visibility": "off"
+             }]
+         }, {
+             "featureType": "administrative.neighborhood",
+             "stylers": [{
+                 "visibility": "off"
+             }]
+         }, {
+             "featureType": "poi",
+             "stylers": [{
+                 "visibility": "off"
+             }]
+         }, {
+             "featureType": "poi.business",
+             "stylers": [{
+                 "visibility": "off"
+             }]
+         }, {
+             "featureType": "poi.business",
+             "elementType": "geometry",
+             "stylers": [{
+                 "visibility": "off"
+             }]
+         }, {
+             "featureType": "road",
+             "elementType": "labels.icon",
+             "stylers": [{
+                 "visibility": "off"
+             }]
+         }, {
+             "featureType": "road.arterial",
+             "stylers": [{
+                 "visibility": "off"
+             }]
+         }, {
+             "featureType": "road.highway",
+             "elementType": "labels",
+             "stylers": [{
+                 "visibility": "off"
+             }]
+         }, {
+             "featureType": "road.local",
+             "stylers": [{
+                 "visibility": "off"
+             }]
+         }, {
+             "featureType": "transit",
+             "stylers": [{
+                 "visibility": "off"
+             }]
+         }]
+     }
 
      map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
      var flooded = new google.maps.KmlLayer({
